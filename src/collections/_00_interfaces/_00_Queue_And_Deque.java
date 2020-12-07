fa4bebd5e1f56c3ca4480c28457d879495acd913
antorpo <antonio.gonzalezr@udea.edu.co>
2020-12-06 23:59:44 -0500

    Queue and Deque

--- src/collections/_00_interfaces/_00_Queue_And_Deque.java
+++ src/collections/_00_interfaces/_00_Queue_And_Deque.java
@@ -4,10 +4,13 @@ package collections._00_interfaces;
     Interface Queue<E>
     public interface Queue<E> extends Collection<E>
 
+    Queue ->
+
     Interface Deque<E>
     public interface Deque<E> extends Queue<E>
 
-    
+    Deque -> 
+
 */
 
 public class _00_Queue_And_Deque {
